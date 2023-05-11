480b4c4dda3585d3ae2f06a507d9e8d78c6f93bf
melissarsousa <melissa.rios.sousa@gmail.com>
2023-05-11 09:06:07 -0300

    Comandos adicionados no teste login_test.js

--- login_test.js
+++ login_test.js
@@ -1,13 +1,26 @@
+var assert = require('assert')
+
 Feature('login');
 
-Scenario('test something',  ({ I }) => {
+Scenario('Teste de Cadastro', async ({ I }) => {
 
-    I.amOnPage('/');
-    I.click({xpath: '//*[@id="top_header"]/div/div/div[2]/div/ul/li[1]/a'});
-    I.waitForElement('#btnLogin', 15); // secs
-    I.fillField('#user', 'mel@gmail.com')
-    I.fillField('#password', 'willleoncio')
-    I.click('#btnLogin');
-    I.see('Login realizado');
+    I.amOnPage('/')
+    I.click({xpath: '//*[@id="top_header"]/div/div/div[2]/div/ul/li[2]/a'})
+    I.waitForElement('#btnRegister', 10) // secs
+    I.fillField('#user', 'Melissa')
+    I.fillField('#email', 'mel@gmail.com')
+    I.fillField('#password', 'bsoleocio')
+    I.click('#btnRegister')
+
+    var title = await I.grabTitle()
+    assert.equal(title, 'QAZANDO Shop E-Commerce')
+
+    I.see('Cadastro realizado!')
+
+    I.waitForElement({xpath: '/html/body/div[2]/div/div[6]/button[1]'})
+    I.click({xpath: '/html/body/div[2]/div/div[6]/button[1]'})
+
+    var paginaUsuario = await I.grabTextFrom('.active')
+    assert.equal(paginaUsuario, 'Dashboard')
 
     });
